2c88aa643ff4fdfbb4bf3cdbad201887ae97a71c
HarryXu497 <harryxu497@gmail.com>
2023-08-26 13:59:13 -0400

    created primitive image optimization component for avif and jpg (maybe webp later) files

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,26 @@
+<script lang="ts"> 
+	export let src: string;
+	export let alt: string = "";
+</script>
+
+<picture>
+	<source srcset="{src}.avif" type="image/avif">
+	<img
+	src="{src}.jpg"
+	alt="{alt}"
+	/>
+</picture>
+
+<style lang="scss">
+	picture {
+		width: 100%;
+	}
+
+    img, source {
+		/* position: absolute; */
+
+		width: 100%;
+		height: 100%; 
+		object-fit: cover;
+    }
+</style>
