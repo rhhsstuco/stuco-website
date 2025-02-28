37163e3ddc0c570d995869b4e2456ef385c18122
Harry Xu <harryxu497@gmail.com>
2025-02-28 09:26:56 -0500

    chnage

--- src/routes/(app)/bhm/6fc7768c-f3c0-46dd-bc9e-40b166ea6ed4/+page.svelte
+++ src/routes/(app)/bhm/6fc7768c-f3c0-46dd-bc9e-40b166ea6ed4/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     import Quiz from "$lib/components/Quiz.svelte";
 
-    const correctAnswers = new Set(["Justice Murray Sinclair", "Murray Sinclair"].map(str => str.toLocaleLowerCase()));
+    const correctAnswers = new Set(["Lincoln Alexander"].map(str => str.toLocaleLowerCase()));
 </script>
 
 <Quiz
     title="Question 2"
     index={2}
-    question="Who was the first Black Chief Justice person in Canada?"
+    question="Which Black Canadian politician became the first Black federal member of Parliament in 1968?"
     nextLocation="Go to the room 2029"
     {correctAnswers}
 />
